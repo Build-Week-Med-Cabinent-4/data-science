994e891eab1e5f5f60e660578997008827dceb24
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2020-10-22 21:10:57 -0400

    Fixed format issues

--- app/api/db_model.py
+++ app/api/db_model.py
@@ -21,39 +21,45 @@ Base = declarative_base()
 
 
 class Strains(Base):
-   '''Creates a table containing all the strains utilized in
-    the creation of the NLP machine learning model used to return the API responce'''
-    
-    __tablename__ = 'strains'
+  '''
+  Creates a table containing all the strains utilized in
+  the creation of the NLP machine learning model used to return the API responce
+  '''
 
-    ID = Column(Integer, primary_key=True)
-    Strain_Name = Column(String(55), nullable=False)
-    Strain_Type = Column(String(55))
-    Description = Column(String)
-    Effect = Column(String)
-    Ailment = Column(String)
-    Flavor = Column(String)
+  __tablename__ = 'strains'
+
+  ID = Column(Integer, primary_key=True)
+  Strain_Name = Column(String(55), nullable=False)
+  Strain_Type = Column(String(55))
+  Description = Column(String)
+  Effect = Column(String)
+  Ailment = Column(String)
+  Flavor = Column(String)
 
 
 class InputsDB(Base):
-  '''Creates a table within the database to store user inputs
-    for use for later marketing and visualization purposes'''
-  
-    __tablename__ = 'inputs_db'
+  '''
+  Creates a table within the database to store user inputs
+  for use for later marketing and visualization purposes
+  '''
 
-    ID = Column(Integer, primary_key=True)
-    Ailment_In = Column(String, nullable=False)
-    Flavor_In = Column(String)
-    Effects_In = Column(String)
+  __tablename__ = 'inputs_db'
+
+  ID = Column(Integer, primary_key=True)
+  Ailment_In = Column(String, nullable=False)
+  Flavor_In = Column(String)
+  Effects_In = Column(String)
 
 db = SessionLocal()
 
 if not engine.dialect.has_table(engine, 'strains'):
     Base.metadata.create_all(bind=engine)
 
-'''Needed to comment out the code below after creating the database with
+'''
+Needed to comment out the code below after creating the database with
 the static csv dataset to prevent duplication. This part of the function
-will need to be researched more to find a better option.'''
+will need to be researched more to find a better option.
+'''
 # url = 'https://raw.githubusercontent.com/Build-Week-Med-Cabinent-4/data-science/main/data/clean/merged_dataset.csv'
 
 # df = pd.read_csv(url)
@@ -75,28 +81,30 @@ will need to be researched more to find a better option.'''
 
 
 def get_db():
-    '''This function will return the data from database.'''
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
+  '''
+  This function will return the data from database.
+  '''
+  try:
+      db = SessionLocal()
+      yield db
+  finally:
+      db.close()
 
 
 # Set the route for the strains table
 @router.get('/strains', response_model=List[schemas.Strains])
 async def show_strains(db: Session=Depends(get_db)):
-    '''
-    View the strain table information we have in our database.
+  '''
+  View the strain table information we have in our database.
 
-    Features in this table include:
-    * **ID**: The id number assigned to the strain (Primary Key)
-    * **Strain_Name**: The name of the strain
-    * **Strain_Type**: The type of strain
-    * **Effect**: What kind of effect(s) know for the strain
-    * **Ailment**: What ailment(s) the strain is known for helping
-    * **Flavor**: A description of the flavor associated with the strain
-    * **Description**: Describes the strain
-    '''
-    strains = db.query(Strains).all()
-    return strains
+  Features in this table include:
+  * **ID**: The id number assigned to the strain (Primary Key)
+  * **Strain_Name**: The name of the strain
+  * **Strain_Type**: The type of strain
+  * **Effect**: What kind of effect(s) know for the strain
+  * **Ailment**: What ailment(s) the strain is known for helping
+  * **Flavor**: A description of the flavor associated with the strain
+  * **Description**: Describes the strain
+  '''
+  strains = db.query(Strains).all()
+  return strains
